57e050534f524c43d7af084cf732423e5e0ba292
Affanzuhaib <affanzuhaibuddin@gmail.com>
2023-10-04 14:17:07 +0530

    c

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,4 +1,4 @@
-import homePage from "./images/HomePage.png";
+import homePage from "./images/homePage.png";
 import logo from "./images/logo.png"
 import profileImg from "./images/new_photo2.jpg"
 import gameplay from "./images/Gameplay2.png"
